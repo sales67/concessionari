11e0aabc120be69eca6a64cd72e19c3ca4b1543e
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-05-18 15:27:47 +0200

    més cosses

--- src/app/cotxes/marca/addMarca/addMarca.component.ts
+++ src/app/cotxes/marca/addMarca/addMarca.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 //import {StateService} from '../common/state.service';
-import {MarcaService} from 'app/cotxes/marca/marca.service';
+import {MarcaService} from '../marca/marca.service';
 import {Http, Headers,Response} from '@angular/http';
 import {Routes,Router} from '@angular/router';
 import { Marca }   from './marca';
