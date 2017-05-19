201ca2cabdeb289a091f77affc6abea8c8fd97e0
ITE-JMALAGARRIG\jmalagarriga <r11j14@gmail.com>
2017-05-19 11:42:46 +0200

    he canviat localhost per ip

--- src/app/cotxes/equipament/equipament.service.ts
+++ src/app/cotxes/equipament/equipament.service.ts
@@ -11,33 +11,20 @@ import { Equipament } from './equipament';
 @Injectable()
 export class EquipamentService {
     
+    myURL = 'http://172.17.0.191:8080';
+
     constructor(private http: Http){ }
     
     equipamentBuit(): Equipament {
-        return new Equipament(
-            
-        );
+        return new Equipament();
     }
     
-    /*public id: number;
-    public name: string;
-    public combustible: string;
-    public potencia: number;
-    public traccio: string;
-    public climatitzador: boolean;
-    public llantes17: boolean;
-    public llantes18: boolean;
-    public llantes19: boolean;
-    public navegador: boolean;
-    public pintura: boolean;
-    public multimedia6: boolean;
-    public multimedia9: boolean;*/
-
-    
     getEq(id) {
-      var a = this.http.get('http://localhost:8080/demo/unXid?id=' + id)
+      var a = this.http.get(this.myURL + '/demo/unXid?id=' + id)
       .map(res => res.json())
       return a;
     }
+
+    
 
 }
