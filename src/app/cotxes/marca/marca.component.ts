1d428676a8bf3e61e8af32fa8fa1caca558eb3b5
ITE-SSALES\ssales <salesvila67@gmail.com>
2017-05-22 12:33:55 +0200

    Neteja de Codi

--- src/app/cotxes/marca/marca.component.ts
+++ src/app/cotxes/marca/marca.component.ts
@@ -1,10 +1,7 @@
-﻿import {Component, OnInit} from '@angular/core';
-//import {StateService} from '../common/state.service';
+import {Component, OnInit} from '@angular/core';
 import {MarcaService} from './marca.service';
 import {Http, Headers,Response} from '@angular/http';
 import {Routes,Router} from '@angular/router';
-import { Marca }   from './marca';
-import {Model} from './model';
 
 
 @Component({
@@ -16,30 +13,5 @@ import {Model} from './model';
 
 
 export class MarcaComponent {
-    message: string;
-    errorMessage: string;
-    nom: any;
-    marques: Marca;
-    models: Model;
-    idMarca;
-    addName;
-    addConce;
-    addCountry;
-    addYear;
-    idMarca2;
-    addName2;
-    addConce2;
-    addCountry2;
-    addYear2;
-    addId3;
-    idMarca3;
-    addName3;
-    addConce3;
-    addCountry3;
-    addYear3;
-
-
     constructor(private marcaService: MarcaService) { }
-
 }
-
--- src/app/cotxes/marca/marca.ts
+++
@@ -1,7 +0,0 @@
-export class Marca {
-    public idConce: number;
-    public name: string;
-  	public any: number;
-  	public pais: string;
-  	public id: number;
-}
