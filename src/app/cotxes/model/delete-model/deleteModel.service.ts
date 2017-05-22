e950c7f0b2096720de3b04d3e31af7fcf05923ed
ITE-JCALLE\jcalle <jcalle@teknos.cat>
2017-05-22 14:33:51 +0200

    Primers passos del deleteByMarca

--- src/app/cotxes/model/delete-model/deleteModel.service.ts
+++ src/app/cotxes/model/delete-model/deleteModel.service.ts
@@ -9,6 +9,7 @@ import {sprintf} from "sprintf-js";
 
 export class deleteModelService{
 private delUrl = 'http://localhost:172.17.0.242/demo/remove?id=';
+private delMarcaUrl = 'http://localhost:172.17.0.242/demo/getModelXmarca?marca=';
 
 constructor(private http: Http){}
     
@@ -16,4 +17,9 @@ deleteModel(deleteId){
         var a = this.http.delete(this.delUrl+ deleteId);
     }
 
+
+deleteByMarca(deleteMarca){
+	var list = this.http.get(this.delMarcaUrl+ deleteMarca);
+
+	}
 }
